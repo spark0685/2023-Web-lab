e8efb546fddfe7a7c7777a9fda7218df107416d0
spark0685 <spark@mail.ustc.edu.cn>
2023-10-09 16:24:39 +0800

    enable movie name spidering

--- movie/movie_spider.py
+++ movie/movie_spider.py
@@ -29,105 +29,9 @@ class Movie_Crawler(object):
         content=get_content.text
         self.soup=BeautifulSoup(content,"html.parser")
         return self.soup
-    def get_movie_name(self):
-        all_movie_name=self.soup.findAll("span",attrs={"property":"v:itemreviewed"})
-        if(len(all_movie_name)==0):
-            return "Not find"
-        else:
-            return all_movie_name[0].string
-    # def get_basic_info(self):
-    #     soup=self.get_soup()
-    #     basic_info=str(soup.findAll("div",attrs={"id":"info"})).strip()
-    #     return basic_info
-    # def get_author_name(self):
-    #     book_info=self.get_basic_info()
-    #     author_index=re.compile(r'.*?作者.*?<a.*?href.*?">(.*?)<',re.S)
-    #     author=re.findall(author_index,book_info)
-    #     if(len(author)==0):
-    #         return "无"
-    #     else:
-    #         return author[0]
-    # def get_press_name(self):
-    #     book_info=self.get_basic_info()
-    #     press_index=re.compile(r'.*?出版社.*?<a.*?href.*?">(.*?)<',re.S)
-    #     press=re.findall(press_index,book_info)
-    #     if(len(press)==0):
-    #         return "无"
-    #     else:
-    #         return press[0]
-    # def get_translator_name(self):
-    #     book_info=self.get_basic_info()
-    #     translator_index=re.compile(r'.*?译者.*?<a.*?href.*?">(.*?)<',re.S)
-    #     translator=re.findall(translator_index,book_info)
-    #     if(len(translator)==0):
-    #         return "无"
-    #     else:
-    #         return translator[0]
-    # def get_pub_year(self):
-    #     book_info=self.get_basic_info()
-    #     pub_index=re.compile(r'.*?出版年.*?pan>(.*?)<b',re.S)
-    #     pub_year=re.findall(pub_index,book_info)
-    #     if(len(pub_year)==0):
-    #         return "无"
-    #     else:
-    #         return pub_year[0]
-    # def get_related_info(self):
-    #     soup=self.get_soup()
-    #     related_info=str(soup.findAll("div",attrs={"class":"related_info"})).strip()
-    #     return related_info
-    # def get_book_intro(self):
-    #     soup=self.get_soup()
-    #     book_info=soup.findAll("div",attrs={"class":"indent","id":"link-report"})
-    #     ans=""
-    #     if(book_info is None):
-    #         return "无"
-    #     for index in book_info:
-    #         book_intro=index.findAll("p")
-    #         if(book_intro is None):
-    #             continue
-    #         for info_string in book_intro:
-    #             if((info_string is None)==False):
-    #                 if(info_string.string is None):
-    #                     continue
-    #                 ans+=info_string.string
-    #     if(len(ans)==0):
-    #         return "无"
-    #     else:
-    #         return ans
-    # def get_author_intro(self):
-    #     soup=self.get_soup()
-    #     book_info=soup.findAll("div",attrs={"class":"indent","id":""})
-    #     ans=""
-    #     for index in book_info:
-    #         info=index.findAll("div",attrs={"class":"intro"})
-    #         if(info is None):
-    #             continue
-    #         for info_string in info:
-    #             intro=info_string.findAll("p")
-    #             if(intro is None):
-    #                 continue
-    #             else:
-    #                 for intro_string in intro:
-    #                     if(intro_string.string is None):
-    #                         continue
-    #                     ans+=intro_string.string
-    #     if(len(ans)==0):
-    #         return "无"
-    #     else:
-    #         return ans
-    # def get_rating(self):
-    #     soup=self.get_soup()
-    #     #<strong class="ll rating_num " property="v:average"> 8.6 </strong>
-    #     rating=soup.findAll("strong",attrs={"property":"v:average"})
-    #     if(rating is None):
-    #         return "无"
-    #     else:
-    #         if(len(rating)==0):
-    #             return "无"
-    #         return rating[0].string
     def file_write_first(self):
         # fieldname=["id","书名","作者","出版社","译者","出版年","内容简介","作者简介","豆瓣评分"]
-        fieldname=["id","电影名"]
+        fieldname=["id","片名","上映年份","评分","评价人数","导演","编剧","主演","类型","国家/地区","语言","时长"]
         with open(self.dest_filename,'w',encoding="utf_8_sig",newline='') as destFile:
             destFile_csv=csv.DictWriter(destFile,fieldnames=fieldname)
             destFile_csv.writeheader()
@@ -135,7 +39,7 @@ class Movie_Crawler(object):
         info_list=[]
         id_list=self.load_id()
         # fieldname=["id","书名","作者","出版社","译者","出版年","内容简介","作者简介","豆瓣评分"]
-        fieldname=["id","电影名"]
+        fieldname=["id","片名","上映年份","评分","评价人数","导演","编剧","主演","类型","国家/地区","语言","时长"]
         with open(self.id_file,'r') as id_file:
             idnum=int(id_file.read())
         with open(self.dest_filename,'a',encoding="utf_8_sig",newline='') as destFile:
@@ -143,33 +47,71 @@ class Movie_Crawler(object):
             for i in range(idnum,len(id_list)):
                 self.url="https://movie.douban.com/subject/"+id_list[i]
                 print(self.url)
-                self.get_soup()
+                soup = self.get_soup()
                 if(self.status_code!=200):
                     with open(self.id_file,'w') as id_file:
                         id_file.write(str(i))
                         print("\nYour request has been denied\n")
                         return
+                # 片名
+                name = soup.find(attrs={'property': 'v:itemreviewed'}).text.split(' ')[0]
+                # 上映年份
+                year = soup.find(attrs={'class': 'year'}).text.replace('(','').replace(')','')
+                # 评分
+                score = soup.find(attrs={'property': 'v:average'}).text
+                # 评价人数
+                votes = soup.find(attrs={'property': 'v:votes'}).text
+                infos = soup.find(attrs={'id': 'info'}).text.split('\n')[1:11]
+                # 导演
+                director = infos[0].split(': ')[1]
+                # 编剧
+                scriptwriter = infos[1].split(': ')[1]
+                # 主演
+                actor = infos[2].split(': ')[1]
+                # 类型
+                filmtype = infos[3].split(': ')[1]
+                # 国家/地区
+                area = infos[4].split(': ')[1]
+                if '.' in area:
+                    area = infos[5].split(': ')[1].split(' / ')[0]
+                else:
+                    area = infos[4].split(': ')[1].split(' / ')[0]
+                # 语言
+                language = infos[5].split(': ')[1].split(' / ')[0]
+                if '大陆' in area or '香港' in area or '台湾' in area:
+                    area = '中国'
+                if '戛纳' in area:
+                    area = '法国'
+                # 时长
+                times0 = soup.find(attrs={'property': 'v:runtime'}).text
+                times = re.findall('\d+', times0)[0]
                 info_list.append(str(id_list[i]))
-                info_list.append(self.get_movie_name())
+                info_list.append(name)
+                info_list.append(year)
+                info_list.append(score)
+                info_list.append(votes)
+                info_list.append(director)
+                info_list.append(scriptwriter)
+                info_list.append(actor)
+                info_list.append(filmtype)
+                info_list.append(area)
+                info_list.append(language)
+                info_list.append(times)
                 print (info_list)
-                # info_list.append(self.get_author_name())
-                # info_list.append(self.get_translator_name())
-                # info_list.append(self.get_pub_year())
-                # info_list.append(self.get_press_name())
-                # info_list.append(self.get_book_intro())
-                # info_list.append(self.get_author_intro())
-                # info_list.append(self.get_rating())
                 destFile_csv.writerow(
                     {
                         "id":info_list[0],
-                        "电影名":info_list[1]
-                        # "作者":info_list[2],			
-                        # "译者":info_list[3],
-                        # "出版年":info_list[4],
-                        #   "出版社":info_list[5],
-                        # "内容简介":info_list[6],
-                        # "作者简介":info_list[7],
-                        # "豆瓣评分":info_list[8]
+                        "片名":info_list[1],
+                        "上映年份":info_list[2],
+                        "评分":info_list[3],
+                        "评价人数":info_list[4],
+                        "导演":info_list[5],
+                        "编剧":info_list[6],
+                        "主演":info_list[7],
+                        "类型":info_list[8],
+                        "国家/地区":info_list[9],
+                        "语言":info_list[10],
+                        "时长":info_list[11]
                     }
                 )
                 info_list.clear()
