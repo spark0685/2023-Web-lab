76c8da2ad9f4fe188ce5f1aba41924fd39f39677
Lucy8179 <132745239+Lucy8179@users.noreply.github.com>
2023-12-24 20:23:06 +0800

    Update kg_final.py

--- lab2/stage2/kg_final.py
+++ lab2/stage2/kg_final.py
@@ -6,14 +6,29 @@ with open('movie_id_map.txt','r') as f:
     for line in f:
         line = line.strip()
         list = line.split('\t')
-        # print(list[1])
         movie_id[list[0]] = list[1]
 with open('douban2fb.txt', 'r') as f:
     for line in f:
         line=line.strip()
         list = line.split('\t')
         movie_entity[list[1]]=movie_id[list[0]]
+        
 Graph=pd.read_csv(filepath_or_buffer="Graph.csv",usecols=['head_entity','relation','tail_entity'])
+tag_Graph=pd.DataFrame({"head_entity":[],"relation":[],"tail_entity":[]})
+write_list=[]
+i=0
+with open("Movie_tag.csv",encoding='utf-8') as f:
+    for line in f:
+        line = line.strip('"')
+        list = line.split(',',1)
+        #print(list)
+        if list[0] in movie_id:
+            write_list.append(list[0])
+            write_list.append("tag")
+            write_list.append(list[1])
+            tag_Graph.loc[len(tag_Graph)]=write_list
+            write_list.clear()
+        
 cons_str=r"<http://rdf.freebase.com/ns/"
 writeline=""
 other_entity={}
@@ -56,6 +71,31 @@ with open("kg_final.txt",'w') as f:
         
         writeline=writeline+"\n"
         f.write(writeline)
+
+with open("kg_final.txt",'a') as f:
+    for index,row in tag_Graph.iterrows():
+        writeline=""
+        head_entity=row.iloc[0]
+        relation=row.iloc[1]
+        tail_entity=row.iloc[2]
+        writeline=writeline+str(movie_id[head_entity])
+        writeline=writeline+" "
+        if relation in relation_map:
+            writeline=writeline+str(relation_map[relation])
+        else:
+            val=len(relation_map)
+            relation_map[relation]=val
+            writeline=writeline+str(relation_map[relation])
         
-print(len(movie_entity)+len(other_entity))
+        writeline=writeline+" "
+        if tail_entity in other_entity:
+            writeline=writeline+str(other_entity[tail_entity])
+        else:
+            val=len(other_entity)+len(movie_entity)
+            other_entity[tail_entity]=val
+            writeline=writeline+str(other_entity[tail_entity])
+        
+        writeline=writeline+"\n"
+        f.write(writeline)
+
         
